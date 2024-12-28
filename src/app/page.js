42dd40145b3e23d2246120887e9e2813a662f1fe
Coded76 <0xhope66@example.com>
2024-12-28 23:29:46 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,104 @@
+// pages/index.js
+"use client";
+
+import { useState, useEffect } from "react";
+import { FaBitcoin, FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import { FaXTwitter } from "react-icons/fa6";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isLoading, setIsLoading] = useState(true);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-white text-3xl animate-pulse">
+          <Image
+            src={"/assets/loading_logo.svg"}
+            height={200}
+            width={200}
+            alt={"Loading Image"}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-white min-h-screen">
+      <Navbar />
+      <Hero />
     </div>
   );
 }
+
+const Navbar = () => (
+  <nav className="flex container justify-between items-center p-4 text-white shadow-md">
+    <div className="flex items-center space-x-2">
+      <Image
+        src={"/assets/logo.svg"}
+        height={200}
+        width={200}
+        alt={"Logo Image"}
+      />
+    </div>
+    <div className="flex bg-[#444444] rounded-full p-1">
+      <button className="cursor-pointer px-4 py-2 rounded-full bg-[#141414] text-white">
+        Home
+      </button>
+      <button className="cursor-pointer px-4 py-2 rounded-full text-gray-300">
+        WhitePaper
+      </button>
+    </div>
+    <button className="flex items-center gap-4 px-4 py-2 rounded-full ">
+      <span>Join</span>
+      <Link href={"#"}>
+        <FaXTwitter />
+      </Link>
+      <Link href={"#"}>
+        <FaPaperPlane />
+      </Link>
+    </button>
+  </nav>
+);
+
+const Hero = () => (
+  <section className="flex container rounded-3xl flex-col items-center justify-center text-center min-h-screen bg-[#141414]  text-white">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.8 }}
+      className="text-6xl mb-4"
+    >
+      <Image
+        src={"/assets/hero.svg"}
+        height={200}
+        width={200}
+        alt={"Hero Image"}
+      />
+    </motion.div>
+    <motion.h1
+      initial={{ y: 50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1, delay: 0.5 }}
+      className="text-4xl font-bold mb-4"
+    >
+      Made For all Bitcoin Lovers
+    </motion.h1>
+    <motion.button
+      initial={{ y: 50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1, delay: 1 }}
+      className="mt-4 px-6 py-3 bg-[#FFFDD0] text-black font-semibold rounded-full hover:bg-[#FFFDD5]"
+    >
+      Buy $blackbitcoin
+    </motion.button>
+  </section>
+);
